fdd226f255d037cb039c79fdd0b7b729c04bcd82
younes-uca <y.zouani@uca.ma>
2024-07-29 15:53:27 +0100

    Zyn init

---
+++ backend-ms1/src/main/java/ma/zs/zyn/dao/specification/core/payement/PaimentCollaboratorStateSpecification.java
@@ -0,0 +1,26 @@
+package  ma.zs.zyn.dao.specification.core.payement;
+
+import ma.zs.zyn.dao.criteria.core.payement.PaimentCollaboratorStateCriteria;
+import ma.zs.zyn.bean.core.payement.PaimentCollaboratorState;
+import ma.zs.zyn.zynerator.specification.AbstractSpecification;
+
+
+public class PaimentCollaboratorStateSpecification extends  AbstractSpecification<PaimentCollaboratorStateCriteria, PaimentCollaboratorState>  {
+
+    @Override
+    public void constructPredicates() {
+        addPredicateId("id", criteria);
+        addPredicate("code", criteria.getCode(),criteria.getCodeLike());
+        addPredicate("libelle", criteria.getLibelle(),criteria.getLibelleLike());
+        addPredicate("style", criteria.getStyle(),criteria.getStyleLike());
+    }
+
+    public PaimentCollaboratorStateSpecification(PaimentCollaboratorStateCriteria criteria) {
+        super(criteria);
+    }
+
+    public PaimentCollaboratorStateSpecification(PaimentCollaboratorStateCriteria criteria, boolean distinct) {
+        super(criteria, distinct);
+    }
+
+}
